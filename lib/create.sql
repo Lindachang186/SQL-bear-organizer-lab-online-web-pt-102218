79f8a0778a617e14a33daf286a3eafd34dbd3a0a
Linda Chang <lindachang186@gmail.com>
2019-01-10 18:42:13 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
 CREATE TABLE bears (
-  id    INTEGER
-  name  TEXT
-  age   INTEGER
-  gender  text
-  color text
-  temperament text
-  alive boolean
+  id INTEGER
+  name TEXT
+  age INTEGER
+  gender TEXT
+  color TEXT
+  temperament TEXT
+  alive BOOLEAN
 );
